b7a2cc89a5bad2c259e35c1f47f93cc5b78941aa
Ilya Kasperovich <kasper.ii2108@gmail.com>
2019-09-09 13:38:57 +0300

    added Abstract class, Errors

--- main.js
+++ main.js
@@ -2,120 +2,256 @@ class ToDoApplication {
   constructor() {}
 
   execute() {
-    let taskManager = new TaskManager(new Store(), new Render());
-    taskManager.createTask('go to school');
-    taskManager.createTask('go to shop');
-    taskManager.createTask('go to gym');
-    let testId = taskManager._store._store[0].getId();
-    taskManager.removeTask(testId);// удалили 'go to school'
+    const store = new StoreLS();
+    const render = new Render();
+    const taskManager = new TaskManager(store);
+    const toDo = new ToDo(taskManager, render);
 
+    let titleInputRef = document.getElementById("title-input");
+    let createTaskBtnRef = document.getElementById("create-btn");
+    let debugBtnRef = document.getElementById("debug-btn");
 
+    createTaskBtnRef.addEventListener("click", () => {
+      toDo.addTask(titleInputRef.value);
+    });
+
+    debugBtnRef.addEventListener("click", () => {
+      toDo.init();
+    });
+
+    toDo.init();
+  }
+}
+
+class ToDo {
+  constructor(taskManager, render) {
+    this._taskManager = taskManager;
+    this._render = render;
+  }
+
+  init() {
+    let tasks = this._taskManager.getTasks();
+    tasks.forEach(task => {
+      this._render.renderTask(task);
+    });
+  }
+
+  addTask(title) {
+    let task = this._taskManager.createTask(title);
+    this._render.renderTask(task);
   }
 }
 
 class Task {
-  constructor(title) {
+  constructor(id, title, completed = false, creationMoment = Date.now()) {
+    this._id = id;
     this._title = title;
-    this._completed = false;
-    this._id = this.makeid();
+    this._completed = completed;
+    this._creationMoment = creationMoment;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get completed() {
+    return this._completed;
+  }
+
+  get creationMoment() {
+    return this._creationMoment;
   }
 
   isCompleted() {
     return this._completed;
   }
 
-  makeid(length = 10) {
-    let result = "";
-    let characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-  }
-
-  getId() {
-    return this._id;
-  }
-
   touggle() {
-    this._completed ? (this._completed = false) : (this._completed = true);
+    this._completed = !this._completed;
+  }
+
+  static toJSON(task) {
+    return JSON.stringify({
+      id: task.id,
+      title: task.title,
+      completed: task.completed,
+      creationMoment: task.creationMoment
+    });
+  }
+
+  static fromJSON(json) {
+    let obj = null;
+    try {
+      obj = JSON.parse(json);
+    } catch (error) {
+      throw new Error("invalid json: ${json}", error.message);
+    }
+
+    return new Task(obj.id, obj.title, obj.completed, obj.creationMoment);
   }
 }
 
 class TaskManager {
-  constructor(store, render) {
+  constructor(store) {
+    if (!(store instanceof AbstractStore)) {
+      throw new Error("Store should be implements AbstractStore interface");
+    }
     this._store = store;
-    this._render = render;
   }
 
   createTask(title) {
-    let newTask = new Task(title);
-    this._store.createTask(newTask);
-    this._render.rederTask(newTask);
+    let id = Math.random()
+      .toString(36)
+      .substring(2, 16);
+    let task = new Task(id, title);
+    this._store.saveTask(task);
+    return task;
   }
 
-  removeTask(id){
+  getTasks() {
+    return this._store.getTasks();
+  }
+
+  removeTask(id) {
     this._store.removeTask(id);
     this._render.dispose(this._store.getTasks());
   }
 }
 
-class Store {
-  constructor() {
-    this._store = [];
-  }
-
-  createTask(task) {
-    this._store.push(task);
-  }
-
-  removeTask(id) {
-    for (let i = 0; i < this._store.length; i++) {
-      if (this._store[i].getId() === id) {
-        this._store.splice(i, 1);
-      }
-    }
+class AbstractStore {
+  getTask(id) {
+    throw new Error("Not implemented");
   }
 
   getTasks() {
-    return this._store;
+    throw new Error("Not implemented");
+  }
+
+  saveTask(task) {
+    throw new Error("Not implemented");
+  }
+}
+
+class StoreLS extends AbstractStore {
+  constructor() {
+    super();
+    this._prefix = "strLS";
   }
 
   getTask(id) {
-    for (let i = 0; i < this._store.length; i++) {
-      if (this._store[i].getId() === id) {
-        return this._store[i];
+    let key = this._prefix + id;
+    const taskJson = localStorage.getItem(key);
+    if (!taskJson) {
+      throw new Error("There is no task with id = ${id}");
+    }
+
+    let task = null;
+    try {
+      task = Task.fromJSON(taskJson);
+    } catch (error) {
+      throw new Error("impossible get task with id = ${id}", error.message);
+    }
+
+    return task;
+  }
+
+  getTasks() {
+    const tasks = [];
+    for (let index = 0; index < localStorage.length; index++) {
+      const key = localStorage.key(index);
+
+      if (key.includes(this._prefix)) {
+        let task = null;
+        try {
+          task = Task.fromJSON(localStorage.getItem(key));
+        } catch (error) {
+          throw new Error("impossible get task with id = ${id}", error.message);
+        }
+        tasks.push(task);
+      }
+    }
+    return tasks;
+  }
+
+  saveTask(task) {
+    let key = this._prefix + task.id;
+    const json = Task.toJSON(task);
+    localStorage.setItem(key, json);
+
+    let taskCopy = null;
+    try {
+      taskCopy = Task.fromJSON(localStorage.getItem(key));
+    } catch (error) {
+      throw new Error("impossible get task with id = ${id}", error.message);
+    }
+    return taskCopy;
+  }
+}
+
+class Store extends AbstractStore {
+  constructor() {
+    super();
+    this._storage = [];
+  }
+
+  saveTask(task) {
+    this._storage.push(task);
+    return task;
+  }
+
+  getTasks() {
+    return this._storage.map(task => {
+      let taskCopy = null;
+      try {
+        taskCopy = Task.fromJSON(Task.toJSON(task));
+      } catch (error) {
+        throw new Error("impossible get task with id = ${id}", error.message);
+      }
+      return taskCopy;
+    });
+  }
+
+  removeTask(id) {
+    for (let i = 0; i < this._storage.length; i++) {
+      if (this._storage[i].getId() === id) {
+        this._storage.splice(i, 1);
       }
     }
   }
 
+  getTask(id) {
+    const task = this._storage.find(task => task.id === id);
+
+    if (!task) {
+      throw new Error("There is no task with id = ${id}");
+    }
+
+    let taskCopy = null;
+    try {
+      taskCopy = Task.fromJSON(Task.toJSON(task));
+    } catch (error) {
+      throw new Error("impossible get task with id = ${id}", error.message);
+    }
+
+    return taskCopy;
+  }
+
   updateTask(task) {
-    for (let i = 0; i < this._store.length; i++) {
-      if (this._store[i].getId() === task.getId()) {
-        this._store.splice(i, 1, task);
+    for (let i = 0; i < this._storage.length; i++) {
+      if (this._storage[i].getId() === task.getId()) {
+        this._storage.splice(i, 1, task);
       }
     }
   }
 }
 
 class Render {
-  constructor() {}
-
-  rederTask(task) {
+  renderTask(task) {
     console.log(task);
-  }
-
-  updateTask(id) {
-    //????????
-  }
-
-  dispose(tasks) {
-    console.clear();
-    for (let i = 0; i < tasks.length; i++) {
-      console.log(tasks[i]);
-    }
   }
 }
 
